1e6430d8fdbf778a2c587a16cc7719e2b6e917d9
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2021-03-22 01:05:43 +0100

    Simple Remix of Clubhouse
    * Add 4th entry to Journal "Simple Remix of Clubhouse"
    * Fix underline styling in footer link to Twitter
    
    Co-authored-by: Dawid Jankowiak <dawid.jankowiak@holidaycheck.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "./Link"
 
 export function Footer() {
   return (
-    <footer className="flex justify-end mx-auto min-w-full max-w-5xl py-4 mb-2 px-2 font-mono text-sm text-gray-700 underline">
+    <footer className="flex justify-end mx-auto min-w-full max-w-5xl py-4 mb-2 px-2 font-mono text-sm">
       <Link href="https://twitter.com/jankowiak_dawid">@dawidjankowiak</Link>
     </footer>
   )
